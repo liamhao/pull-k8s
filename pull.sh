6150563632581c6a1cc6f43d00b2e98459645b0f
Liam Hao <540930859@qq.com>
2022-03-19 11:37:38 +0800

    Create pull.sh

---
+++ pull.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+k8sReg="k8s.gcr.io"
+myReg="registry.cn-beijing.aliyuncs.com/haosijia"
+
+for i in `kubeadm config images list`; do
+
+  imageNameWithVersion=${i#$k8sReg/}
+
+  imageName=${imageNameWithVersion%\:*}
+  imageVersion=${imageNameWithVersion#*:}
+
+  if [ $imageName = "coredns/coredns" ]; then
+	  docker pull $myReg/coredns:$imageVersion
+    docker tag $myReg/coredns:$imageVersion $k8sReg/$imageNameWithVersion
+	  docker rmi $myReg/coredns:$imageVersion
+  else
+  	docker pull $myReg/$imageNameWithVersion
+    docker tag $myReg/$imageNameWithVersion $k8sReg/$imageNameWithVersion
+  	docker rmi $myReg/$imageNameWithVersion
+  fi
+
+done;
