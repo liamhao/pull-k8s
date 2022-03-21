5ca95fc7fcaf16b1075485d298f132e70e595a61
liamhao <w736611944@gmail.com>
2022-03-21 23:37:55 +0800

    补充

--- uninstall.sh
+++ uninstall.sh
@@ -2,4 +2,5 @@
 
 kubeadm reset -f
 yum remove -y docker docker-common docker-selinux docker-engine docker-ce-cli docker-client containerd.io kubeadm kubectl kubelet
+systemctl daemon-reload
 rm -rf /var/lib/docker
