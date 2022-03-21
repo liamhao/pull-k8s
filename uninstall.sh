383a1bbdda440efe057c5801dbb2096752322422
liamhao <w736611944@gmail.com>
2022-03-21 10:19:46 +0800

    清理时删除docker

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 kubeadm reset -f
-yum remove -y docker docker-common docker-selinux dockesr-engine kubeadm kubectl kubelet
+yum remove -y docker docker-common docker-selinux docker-engine docker-ce-cli docker-client containerd.io kubeadm kubectl kubelet
 rm -rf /var/lib/docker
